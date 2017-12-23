61ef89d80e0e48a63192234df4e7d2c6a93e539d
Kiewan VILLATEL <kiewan.villatel@gmail.com>
2017-12-23 19:17:08 +0100

    Add random forest model

--- titanic/titanic_random_forest.py
+++ titanic/titanic_random_forest.py
@@ -2,12 +2,9 @@ import os
 
 import numpy as np
 import pandas as pd
-from keras import optimizers
-from keras.callbacks import EarlyStopping
-from keras.layers import Dense, Dropout
-from keras.models import Sequential
 from sacred import Experiment
 from sacred.observers import MongoObserver
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 ex = Experiment()
@@ -15,19 +12,11 @@ ex.observers.append(MongoObserver.create(os.environ['mongodb_url'], db_name=os.e
 
 @ex.config
 def my_config():
-  hidden_size = 128
   nb_models = 1
-  dropout_rate = 0
-  batch_size = 32
-  epochs = 1000
-  learning_rate = 0.00001
-  decay = 1e-7
-  validation_split = 0.1
-
   fare_modulo = 5
   age_modulo = 5
 
-ex.add_config('config_mlp.json')
+ex.add_config('config_random_forest.json')
 
 LABEL = 'Survived'
 ID = 'PassengerId'
@@ -73,20 +62,12 @@ def build_datasets(fare_modulo, age_modulo):
 
 
 @ex.capture
-def build_model(hidden_size, input_size, dropout_rate, learning_rate, decay):
-  model = Sequential()
-  model.add(Dense(units=hidden_size, activation='relu', input_dim=input_size))
-  model.add(Dropout(dropout_rate, noise_shape=None, seed=None))
-  model.add(Dense(units=hidden_size, activation='relu', input_dim=hidden_size))
-  model.add(Dropout(dropout_rate, noise_shape=None, seed=None))
-  model.add(Dense(units=2, activation='softmax'))
-  model.compile(loss='binary_crossentropy',
-                optimizer=optimizers.Adam(lr=learning_rate, decay=decay),
-                metrics=['accuracy'])
+def build_model():
+  model = RandomForestClassifier()
   return model
 
 @ex.automain
-def my_main(nb_models, batch_size, epochs, validation_split):
+def my_main(nb_models):
 
   one_hot_features, df_train, df_valid, df_test = build_datasets()
 
@@ -94,14 +75,9 @@ def my_main(nb_models, batch_size, epochs, validation_split):
   df_valid[PREDICTION] = 0
 
   for i in range(nb_models):
-    model = build_model(input_size=len(one_hot_features))
+    model = build_model()
     model.fit(df_train[one_hot_features],
-              pd.get_dummies(df_train[LABEL]),
-              epochs=epochs,
-              batch_size=batch_size,
-              validation_split=validation_split,
-              callbacks=[EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto')],
-              verbose=0)
+              pd.get_dummies(df_train[LABEL]))
     df_test[LABEL] += model.predict(df_test[one_hot_features])[:, 1]
     df_valid[PREDICTION] += model.predict(df_valid[one_hot_features])[:, 1]
 
